37f9a608ef668d83c02504b7bf3475633bc6a4e5
Kevo27 <kevin.taufer@web.de>
2017-10-20 10:37:51 +0200

    .gitignore

---
+++ WebSalon/Controllers/StepsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebSalon.Models;
+
+namespace WebSalon.Controllers
+{
+    public class StepsController : Controller
+    {
+        private SalonEntities db = new SalonEntities();
+
+        // GET: Steps
+        public ActionResult Index()
+        {
+            return View(db.Steps.ToList());
+        }
+
+        // GET: Steps/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Step step = db.Steps.Find(id);
+            if (step == null)
+            {
+                return HttpNotFound();
+            }
+            return View(step);
+        }
+
+        // GET: Steps/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Steps/Create
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StepsID,Title,EstDuration")] Step step)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Steps.Add(step);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(step);
+        }
+
+        // GET: Steps/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Step step = db.Steps.Find(id);
+            if (step == null)
+            {
+                return HttpNotFound();
+            }
+            return View(step);
+        }
+
+        // POST: Steps/Edit/5
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StepsID,Title,EstDuration")] Step step)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(step).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(step);
+        }
+
+        // GET: Steps/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Step step = db.Steps.Find(id);
+            if (step == null)
+            {
+                return HttpNotFound();
+            }
+            return View(step);
+        }
+
+        // POST: Steps/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Step step = db.Steps.Find(id);
+            db.Steps.Remove(step);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
