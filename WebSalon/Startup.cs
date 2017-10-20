9db4ab76b7eb0fbcb49ff27f73d7c6951f2da5c5
Kevo27 <kevin.taufer@web.de>
2017-10-20 10:09:27 +0200

    Initial catalog

---
+++ WebSalon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSalon.Startup))]
+namespace WebSalon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
